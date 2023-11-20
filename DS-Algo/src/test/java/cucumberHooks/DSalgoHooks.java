8761c12f26a5f3fb7f16a08b7dba76ca6ef924c9
DebalinaChakrabarti <debalinachakrabarti@gmail.com>
2023-11-20 00:35:50 -0600

    commented out all code here
    commented out all code here

--- DS-Algo/src/test/java/cucumberHooks/DSalgoHooks.java
+++ DS-Algo/src/test/java/cucumberHooks/DSalgoHooks.java
@@ -5,22 +5,24 @@ import java.time.Duration;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import stepDefinition.BaseClass;
 
 public class DSalgoHooks extends BaseClass {
-	@Before
-	public void setup() {
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.manage().window().maximize();
-	}
+//	@Before
+//	public void setup() {
+//		driver = new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		driver.manage().window().maximize();
+//	}
 
-	@After
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+//	@AfterAll
+//	public void tearDown() {
+//		if (driver != null) {
+//			driver.quit();
+//		}
+//	}
 
 }
